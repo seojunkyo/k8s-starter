113177893c9ad41a2165eec4d33f2045b69aa306
Yongki Kim <cloudeyes@gmail.com>
2020-05-29 18:24:14 +0900

    Fix image

--- README.md
+++ README.md
@@ -3,6 +3,16 @@ Kubernetes Starter Project
 
 Simple Kubernetes starter template for FastAPI developers.
 
+# Instructions
+
+### 1. Setup `sample` namespace
+  ```
+  $ cd k8s
+  $ ./setup.sh
+  ```
+
+### 2. Build your own `sample-service` image.
+
 - `build.sh`: Build docker image.
 - `push.sh` : Build & push docker image.
 - `debug.sh` : Debug generated docker image with a temporary container.
--- sample/database.py
+++ sample/database.py
@@ -2,12 +2,24 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
+ENV = os.environ.get('ENV', 'DEV')
+
+DB_TYPE = os.environ.get('DB_TYPE', 'mysql')
+DB_HOST = os.environ.get('DB_HOST', 'localhost')
+DB_USER = os.environ.get('DB_USER', 'sample')
+DB_PASS = os.environ.get('DB_PASS', 'sample')
+DB_NAME = os.environ.get('DB_NAME', 'sample')
+
 DATABASE_URL = {
     'DEV': 'sqlite:///./dev.db',
     'TEST': 'sqlite:///./test.db',
-}[os.environ.get('ENV', 'DEV')]
-engine = create_engine(DATABASE_URL, connect_args={
+    'PROD': f'{DB_TYPE}://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
+}[ENV]
+
+connect_args = {
     'check_same_thread': False
-})
+} if ENV != 'PROD' else {}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 Base = declarative_base()
